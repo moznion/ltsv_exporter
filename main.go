c3939ef645ad7e2835ab0ed2d97f9f5a9b1caf19
moznion <moznion@gmail.com>
2016-09-04 22:27:26 +0900

    Fix CLI option

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/mkideal/cli"
 	"github.com/prometheus/client_golang/prometheus"
@@ -16,23 +15,19 @@ var (
 )
 
 type opt struct {
-	Help    bool   `cli:"h,help" usage:"display help"`
-	Version bool   `cli:"v,version" usage:"display version and revision"`
-	Port    int    `cli:"p,port" usage:"set port number" dft:"6666"`
-	LTSVURL string `cli:"*u,url" usage:"a URL of the LTSV"`
+	cli.Helper
+	Version bool   `cli:"!v,version" usage:"display version and revision"`
+	Port    int    `cli:"p,port" usage:"set the port number to listen" dft:"6666"`
+	LTSVURL string `cli:"*u,url" usage:"set a URL of the LTSV"`
 }
 
 func Run(args []string) {
 	cli.Run(&opt{}, func(ctx *cli.Context) error {
 		argv := ctx.Argv().(*opt)
-		if argv.Help {
-			ctx.String(ctx.Usage())
-			os.Exit(0)
-		}
 
 		if argv.Version {
 			ctx.String(fmt.Sprintf("%s/%s\n", ver, rev))
-			os.Exit(0)
+			return nil
 		}
 
 		port := argv.Port
